607f83634bae326a2df9b11750f0aaffbeacbf04
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-07-20 18:58:54 +0900

    httpsurvlet

--- src/main/java/org/example/CalculatorServlet.java
+++ src/main/java/org/example/CalculatorServlet.java
@@ -7,17 +7,21 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/calculate")
-public class CalculatorServlet extends GenericServlet {
-
+public class CalculatorServlet extends HttpServlet {
+    /**
+     * 제네릭과 HttpServlet 차이는 get,post 등 메서드별 함수 호출을 따로 가능한가 아닌가이다.
+     */
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
 
-
     @Override
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.info("service");
         int operand1 = Integer.parseInt(request.getParameter("operand1"));
         String operator = request.getParameter("operator");
@@ -28,6 +32,4 @@ public class CalculatorServlet extends GenericServlet {
         PrintWriter writer = response.getWriter();
         writer.println(result);
     }
-
-
 }
